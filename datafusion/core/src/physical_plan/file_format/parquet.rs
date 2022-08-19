0dc64657a3af3eb812d050745d461c1f95ea5549
Dan Harris <dan@coralogix.com>
2022-08-19 13:21:43 -0400

    Update arrow deps

--- datafusion/core/src/physical_plan/file_format/parquet.rs
+++ datafusion/core/src/physical_plan/file_format/parquet.rs
@@ -57,6 +57,9 @@ use futures::future::BoxFuture;
 use futures::{FutureExt, StreamExt, TryFutureExt, TryStreamExt};
 use log::debug;
 use object_store::{ObjectMeta, ObjectStore};
+use parquet::arrow::arrow_reader::{
+    ArrowPredicate, ArrowPredicateFn, ArrowReaderOptions, RowFilter,
+};
 use parquet::arrow::async_reader::AsyncFileReader;
 use parquet::arrow::{ArrowWriter, ParquetRecordBatchStreamBuilder, ProjectionMask};
 use parquet::basic::{ConvertedType, LogicalType};
@@ -388,7 +391,13 @@ impl FileOpener for ParquetOpener {
         let pushdown_filters = self.scan_options.pushdown_filters;
 
         Ok(Box::pin(async move {
-            let mut builder = ParquetRecordBatchStreamBuilder::new(reader).await?;
+            let mut builder = if pruning_predicate.is_some() {
+                let options = ArrowReaderOptions::new().with_page_index(true);
+                ParquetRecordBatchStreamBuilder::new_with_options(reader, options).await?
+            } else {
+                ParquetRecordBatchStreamBuilder::new(reader).await?
+            };
+
             let adapted_projections =
                 schema_adapter.map_projections(builder.schema(), &projection)?;
 
