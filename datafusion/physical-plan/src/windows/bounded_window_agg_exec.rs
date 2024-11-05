d8362f7f7ecfef7d6a9a3e7adca9dd8dc7486d7c
Jagdish Parihar <jatin6972@gmail.com>
2024-11-05 07:51:36 -0700

    Deprecate `LexOrderingRef` and `LexRequirementRef` (#13233) v44
    * converted LexOrderingRef to &LexOrdering
    
    * using  LexOrdering::from_ref fn  instead of directly cloning it
    
    * using as_ref instread of &
    
    * using as_ref
    
    * removed commented code
    
    * updated cargo lock
    
    * updated LexRequirementRef to &LexRequirement
    
    * fixed clippy issues
    
    * fixed taplo error for cargo.toml in physical-expr-common
    
    * removed commented code
    
    * fixed clippy errors
    
    * fixed clippy error
    
    * fixes
    
    * removed  LexOrdering::from_ref instead using clone and created LexOrdering::empty() fn
    
    * Update mod.rs
    
    ---------
    
    Co-authored-by: Berkay Şahin <124376117+berkaysynnada@users.noreply.github.com>
    Co-authored-by: berkaysynnada <berkay.sahin@synnada.ai>
    
    (cherry picked from commit 9005585fa6f4eb6a4d0cc515b6ad76794c33c626)

--- datafusion/physical-plan/src/windows/bounded_window_agg_exec.rs
+++ datafusion/physical-plan/src/windows/bounded_window_agg_exec.rs
@@ -1186,7 +1186,6 @@ mod tests {
     };
     use datafusion_physical_expr::{LexOrdering, PhysicalExpr};
 
-    use datafusion_physical_expr_common::sort_expr::LexOrderingRef;
     use futures::future::Shared;
     use futures::{pin_mut, ready, FutureExt, Stream, StreamExt};
     use itertools::Itertools;
@@ -1553,7 +1552,7 @@ mod tests {
             Arc::new(BuiltInWindowExpr::new(
                 last_value_func,
                 &[],
-                LexOrderingRef::default(),
+                &LexOrdering::default(),
                 Arc::new(WindowFrame::new_bounds(
                     WindowFrameUnits::Rows,
                     WindowFrameBound::Preceding(ScalarValue::UInt64(None)),
@@ -1564,7 +1563,7 @@ mod tests {
             Arc::new(BuiltInWindowExpr::new(
                 nth_value_func1,
                 &[],
-                LexOrderingRef::default(),
+                &LexOrdering::default(),
                 Arc::new(WindowFrame::new_bounds(
                     WindowFrameUnits::Rows,
                     WindowFrameBound::Preceding(ScalarValue::UInt64(None)),
@@ -1575,7 +1574,7 @@ mod tests {
             Arc::new(BuiltInWindowExpr::new(
                 nth_value_func2,
                 &[],
-                LexOrderingRef::default(),
+                &LexOrdering::default(),
                 Arc::new(WindowFrame::new_bounds(
                     WindowFrameUnits::Rows,
                     WindowFrameBound::Preceding(ScalarValue::UInt64(None)),
