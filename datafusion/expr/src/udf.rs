ae543c8db699fc3df6b34c7d8e18873bbd7f7c5f
Christoph Schulze <christoph.schulze@coralogix.com>
2025-01-03 14:35:08 +0100

    WIP

--- datafusion/expr/src/udf.rs
+++ datafusion/expr/src/udf.rs
@@ -479,8 +479,8 @@ pub trait ScalarUDFImpl: Debug + Send + Sync {
     /// If the function is `ABS(a)`, and the input interval is `a: [-3, 2]`,
     /// then the output interval would be `[0, 3]`.
     fn evaluate_bounds(&self, input: &[&Interval]) -> Result<Interval> {
-        let input_data_types: &[DataType] = input.iter().map(|i| i.data_type()).collect();
-        let return_type = self.return_type(input_data_types)?;
+        let input_data_types = input.iter().map(|i| i.data_type()).collect::<Vec<DataType>>();
+        let return_type = self.return_type(&input_data_types)?;
         Interval::make_unbounded(&return_type)
     }
 
