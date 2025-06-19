apache/datafusion

Fix `CoalescePartitionsExec` proto serialization (#15824)
* add fetch to CoalescePartitionsExecNode

* gen proto code

* Add test

* fix

* fix build

* Fix test build

* remove comments

datafusion/proto/src/physical_plan/mod.rs
@@ -792,7 +792,10 @@ impl protobuf::PhysicalPlanNode {
     ) -> Result<Arc<dyn ExecutionPlan>> {
         let input: Arc<dyn ExecutionPlan> =
             into_physical_plan(&merge.input, registry, runtime, extension_codec)?;
-        Ok(Arc::new(CoalescePartitionsExec::new(input)))
+        Ok(Arc::new(
+            CoalescePartitionsExec::new(input)
+                .with_fetch(merge.fetch.map(|f| f as usize)),
+        ))
     }
 
     fn try_into_repartition_physical_plan(
@@ -2354,6 +2357,7 @@ impl protobuf::PhysicalPlanNode {
             physical_plan_type: Some(PhysicalPlanType::Merge(Box::new(
                 protobuf::CoalescePartitionsExecNode {
                     input: Some(Box::new(input)),
+                    fetch: exec.fetch().map(|f| f as u32),
                 },
             ))),
         })

coralogix/arrow-datafusion

datafusion/proto/src/physical_plan/mod.rs
@@ -291,7 +291,10 @@ impl AsExecutionPlan for protobuf::PhysicalPlanNode {
             PhysicalPlanType::Merge(merge) => {
                 let input: Arc<dyn ExecutionPlan> =
                     into_physical_plan(&merge.input, registry, runtime, extension_codec)?;
-                Ok(Arc::new(CoalescePartitionsExec::new(input)))
+                Ok(Arc::new(
+                    CoalescePartitionsExec::new(input)
+                        .with_fetch(merge.fetch.map(|n| n as usize)),
+                ))
             }
             PhysicalPlanType::Repartition(repart) => {
                 let input: Arc<dyn ExecutionPlan> = into_physical_plan(
@@ -1671,6 +1674,7 @@ impl AsExecutionPlan for protobuf::PhysicalPlanNode {
                 physical_plan_type: Some(PhysicalPlanType::Merge(Box::new(
                     protobuf::CoalescePartitionsExecNode {
                         input: Some(Box::new(input)),
+                        fetch: exec.fetch().map(|n| n as u32),
                     },
                 ))),
             });